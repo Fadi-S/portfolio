d322548e3ba84cb80ca5f0fb67ac9724d2f4c10a
Fadi <fadisarwat@yahoo.com>
2020-12-09 06:17:25 +0200

    Edited routes controller syntax

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,12 @@
 <?php
 
+use App\Http\Controllers\{PlanController, ProjectController};
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::resource('project', 'ProjectController')->only('index', 'create', 'store');
+Route::resource('project', ProjectController::class)->only('index', 'create', 'store');
 
-Route::resource('plan', 'PlanController')->only('index', 'create', 'store');
+Route::resource('plan', PlanController::class)->only('index', 'create', 'store');
