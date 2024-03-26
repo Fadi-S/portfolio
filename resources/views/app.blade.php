71d9c5c6210e8cd527140663ba22aefe05e7a4a6
Fadi Sarwat <fadisarwat@yahoo.com>
2024-03-27 00:13:31 +0200

    WIP

---
+++ resources/views/app.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite('resources/js/app.js')
+    @inertiaHead
+    @vite('resources/css/app.css')
+</head>
+<body>
+@inertia
+</body>
+</html>
